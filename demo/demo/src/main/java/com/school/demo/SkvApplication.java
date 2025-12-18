450bc3fe291a4edbbdd826a73a091afedfe43025
koushikknk15 <110097267+koushikknk15@users.noreply.github.com>
2025-12-18 17:39:21 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/com/school/demo/SkvApplication.java
@@ -0,0 +1,13 @@
+package com.school.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkvApplication.class, args);
+	}
+
+}
